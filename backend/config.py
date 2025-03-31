2df28a07d213f5654e231d3e51f49e636464d00b
Hiba Altaf <54900941+hibaa8@users.noreply.github.com>
2025-03-31 08:25:01 -0400

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 
 SECRET_KEY = "secret_key"
-MONGO_URI = "mongodb+srv://admin:1234@cluster0.wwppvqo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB URI
+MONGO_URI = ""  # Replace with your MongoDB URI
 JWT_SECRET_KEY = "jwt_secret"
