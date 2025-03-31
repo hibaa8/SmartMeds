178225d9ddfe6c537782d1c9195c6609c8da4e34
Hiba Altaf <54900941+hibaa8@users.noreply.github.com>
2025-03-31 08:25:29 -0400

    Update app

--- backend/app.py
+++ backend/app.py
@@ -1,30 +1,3 @@
-# from flask import Flask, jsonify
-# from flask_pymongo import PyMongo
-# import os
-# from dotenv import load_dotenv
-# from flask import current_app, g
-# from pymongo import MongoClient
-# from flask_cors import CORS
-
-# mongo = PyMongo()
-# load_dotenv()
-
-# def get_db():
-#     db = getattr(g, "_database", None)
-#     if db is None:
-#         db = g._database = PyMongo(current_app).db
-#     return db
-
-# app = Flask(__name__)
-# # CORS(app) 
-
-# @app.route("/members")
-# def init():
-#     return jsonify({"members":["members"]})
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
 
 from flask import Flask, send_from_directory
 from flask_cors import CORS
@@ -38,11 +11,9 @@ app = Flask(__name__, static_folder="frontend/build", static_url_path="")
 app.config["JWT_SECRET_KEY"] = config.JWT_SECRET_KEY
 jwt = JWTManager(app)
 
-# CORS(app)
-# Allow requests from React frontend
+
 CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 
-# Serve React frontend
 @app.route("/")
 def serve_react():
     return send_from_directory("frontend/build", "index.html")
