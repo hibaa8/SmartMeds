80aa810bafbaee880dcd3d3ad671ee2a76456f90
Hiba Altaf <54900941+hibaa8@users.noreply.github.com>
2025-04-16 02:43:03 -0400

    update configs

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 import os
 
-SECRET_KEY = "secret_key"
-MONGO_URI = ""  # Replace with your MongoDB URI
-JWT_SECRET_KEY = "jwt_secret"
+MONGO_URI=""
+JWT_SECRET_KEY=""
+GOOGLE_VISION_CREDENTIALS_FILE=""
